f912fffefbd89be94ad009b0f093f58347714af5
youngho <xfile0304@gmail.com>
2026-05-01 06:42:14 +0900

    코드 수정 완료: ScoreApplication.java가 SpringBootServletInitializer를 상속하도록 수정하여 외부 Tomcat에서 정상적으로 서블릿 컨텍스트를 로드할 수 있게 했습니다.

--- src/main/java/to/yho/score/ScoreApplication.java
+++ src/main/java/to/yho/score/ScoreApplication.java
@@ -2,11 +2,18 @@ package to.yho.score;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
 @ConfigurationPropertiesScan
-public class ScoreApplication {
+public class ScoreApplication extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(ScoreApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(ScoreApplication.class, args);
